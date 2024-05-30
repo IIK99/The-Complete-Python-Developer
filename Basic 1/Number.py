c6c277fc08676dfda0af7af2f6c6c8ebcec67bdc
IIK99 <muhamad.ikmal1099@gmail.com>
2024-05-30 22:26:20 +0700

    first commit

---
+++ Basic 1/Number.py
@@ -0,0 +1,48 @@
+# Fundamental data types
+# int and float
+print("Data types")
+print(type(5))
+print(type(5.0))
+print(type(5 + 0))
+print(type(5 + 0j))
+print(type(5 / 2))
+print(type(5 // 2))
+print(type(5 % 2))
+print(type(5**2))
+print(type(-5))
+print(type(5.0 + 0j))
+print(type(5.0 / 2))
+print(type(5.0 // 2))
+print(type(5.0 % 2))
+print(type(5.0**2))
+print(type(-5.0))
+
+# Integer
+x = 5
+y = 2
+
+# Operasi aritmatika
+sum_result = x + y  # Penjumlahan
+diff_result = x - y  # Pengurangan
+prod_result = x * y  # Perkalian
+int_div_result = x // y  # Pembagian bulat
+mod_result = x % y  # Modulo
+exp_result = x**y  # Eksponensial
+
+print("Integer Operations:")
+print("Sum:", sum_result)  # Output: 7
+print("Difference:", diff_result)  # Output: 3
+print("Product:", prod_result)  # Output: 10
+print("Integer Division:", int_div_result)  # Output: 2
+print("Modulo:", mod_result)  # Output: 1
+print("Exponent:", exp_result)  # Output: 25
+
+# Float
+a = 5.0
+b = 2.0
+
+# Operasi aritmatika
+float_div_result = a / b  # Pembagian float
+
+print("\nFloat Operations:")
+print("Float Division:", float_div_result)  # Output: 2.5
