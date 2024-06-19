197036e1937bf4f163c54eec7bffcc6dad67300b
IIK99 <muhamad.ikmal1099@gmail.com>
2024-06-19 19:27:57 +0700

    tetap tidak ada yang jalan

--- 13 ML and DS/exercise really smart brain/brain.py
+++ 13 ML and DS/exercise really smart brain/brain.py
@@ -1,19 +1,26 @@
-from imageai.Classification import ImageClassification
 import os
+
+from imageai.Classification import ImageClassification
 
 exec_path = os.getcwd()
 
 prediction = ImageClassification()
 # Menggunakan model ResNet
 prediction.setModelTypeAsResNet50()
-model_path = os.path.join(exec_path, 'resnet50_weights_tf_dim_ordering_tf_kernels.h5')  # Path ke model ResNet
+model_path = os.path.join(
+    exec_path, "resnet50_weights_tf_dim_ordering_tf_kernels.h5"
+)  # Path ke model ResNet
 
 if not os.path.isfile(model_path):
-    raise FileNotFoundError(f"Model file not found at {model_path}. Please ensure the file exists.")
+    raise FileNotFoundError(
+        f"Model file not found at {model_path}. Please ensure the file exists."
+    )
 
 prediction.setModelPath(model_path)
 prediction.loadModel()
 
-predictions, probabilities = prediction.classifyImage(os.path.join(exec_path, 'house.jpg'), result_count=5)
+predictions, probabilities = prediction.classifyImage(
+    os.path.join(exec_path, "house.jpg"), result_count=5
+)
 for eachPred, eachProb in zip(predictions, probabilities):
-    print(f'{eachPred} : {eachProb}')
+    print(f"{eachPred} : {eachProb}")
